ec83a45c7debf363b9610669478f3af89ee7ca21
hauzankhoirullah <hauzan41200@gmail.com>
2023-05-30 10:18:00 +0700

    revisi task query

--- 5_DATABASE_MANAGAMENT/TASK/database_movie/query.sql
+++ 5_DATABASE_MANAGAMENT/TASK/database_movie/query.sql
@@ -1,21 +1,3 @@
-
--- READ MOVIE TITLES
-SELECT title_movie FROM movie;
-
-
--- READ MOVIE TITLES, GENRE, AND RELEASE YEAR WITHOUT STRING_AGG
-SELECT title_movie, date_part('year',release_date_movie) , g.name_genre
-FROM movie m JOIN bridge_movie_genre b ON m.id_movie = b.id_movie
-JOIN genre g ON g.id_genre =  b.id_genre;
-
--- READ MOVIE TITLES, GENRE, AND RELEASE YEAR
-SELECT title_movie, date_part('year',release_date_movie) ,string_agg(name_genre, ', ') 
-FROM movie m JOIN bridge_movie_genre b ON m.id_movie = b.id_movie
-JOIN genre g ON g.id_genre =  b.id_genre
-GROUP BY m.id_movie;
-
-
-
 SELECT * FROM movie m JOIN bridge_movie_genre b 
 ON m.id_movie = b.id_movie JOIN genre g ON g.id_genre =  b.id_genre 
 WHERE name_genre = 'Comedy' AND  date_part('year',release_date_movie) = 2017
