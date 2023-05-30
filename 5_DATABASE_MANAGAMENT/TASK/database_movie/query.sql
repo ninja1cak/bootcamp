669c2ef6709b5017eef282ce05bbe440553d3b6a
hauzankhoirullah <hauzan41200@gmail.com>
2023-05-30 09:45:18 +0700

    revisi task

--- 5_DATABASE_MANAGAMENT/TASK/database_movie/query.sql
+++ 5_DATABASE_MANAGAMENT/TASK/database_movie/query.sql
@@ -13,3 +13,12 @@ SELECT title_movie, date_part('year',release_date_movie) ,string_agg(name_genre,
 FROM movie m JOIN bridge_movie_genre b ON m.id_movie = b.id_movie
 JOIN genre g ON g.id_genre =  b.id_genre
 GROUP BY m.id_movie;
+
+
+
+SELECT * FROM movie m JOIN bridge_movie_genre b 
+ON m.id_movie = b.id_movie JOIN genre g ON g.id_genre =  b.id_genre 
+WHERE name_genre = 'Comedy' AND  date_part('year',release_date_movie) = 2017
+AND title_movie = 'SPIDERMAN: HOMECOMING';
+
+SELECT * FROM movie WHERE title_movie = 'BLACK WIDOW';
